62d1d7194322feb6ff42af222eb5765d8bc477bb
vivianroshan <vivianroshan@gmail.com>
2021-09-29 02:59:03 +0530

    added database object for book

---
+++ BookShareOnline.WebAPI.Data/dbo.Books.data.sql
@@ -0,0 +1,10 @@
+﻿SET IDENTITY_INSERT [dbo].[Books] ON
+INSERT INTO [dbo].[Books] ([Id], [Name], [Price], [Category], [Author], [Details], [Seller], [PhotoFileName]) VALUES (4, N'Organic Chemistry', 630, N'Science', N'mtg', N'jee neet', N'roshan', N'4.png')
+INSERT INTO [dbo].[Books] ([Id], [Name], [Price], [Category], [Author], [Details], [Seller], [PhotoFileName]) VALUES (5, N'The Power of Yoga', 800, N'Science', N'Yamini Muthanna', N'health', N'roshan', N'5.png')
+INSERT INTO [dbo].[Books] ([Id], [Name], [Price], [Category], [Author], [Details], [Seller], [PhotoFileName]) VALUES (6, N'Biology', 1234, N'Science', N'Sirnivasan', N'12th standard preparation', N'ajith', N'6.png')
+INSERT INTO [dbo].[Books] ([Id], [Name], [Price], [Category], [Author], [Details], [Seller], [PhotoFileName]) VALUES (7, N'Children Science', 1234, N'Science', N'Meera', N'For children to have fun with science', N'rohith', N'7.png')
+INSERT INTO [dbo].[Books] ([Id], [Name], [Price], [Category], [Author], [Details], [Seller], [PhotoFileName]) VALUES (8, N'Children Maths', 321, N'Science', N'babuji', N'For children to have fun with Maths', N'adithan', N'8.png')
+INSERT INTO [dbo].[Books] ([Id], [Name], [Price], [Category], [Author], [Details], [Seller], [PhotoFileName]) VALUES (9, N'Rich Dad Poor Dad', 200, N'Lifestyle', N'Robert T. Kiyosaki', N'improve your mindset', N'leander', N'9.png')
+INSERT INTO [dbo].[Books] ([Id], [Name], [Price], [Category], [Author], [Details], [Seller], [PhotoFileName]) VALUES (10, N'45 Laws of Power', 2300, N'Lifestyle', N'Robert Greene', N'the Machiavellian school of knowledge', N'adi', N'10.png')
+INSERT INTO [dbo].[Books] ([Id], [Name], [Price], [Category], [Author], [Details], [Seller], [PhotoFileName]) VALUES (11, N'Path to Creative freedom', 4000, N'Lifestyle', N'', N'', N'adi', N'11.png')
+SET IDENTITY_INSERT [dbo].[Books] OFF
